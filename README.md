78b9022b4ab48cb160225b7c9426787e67b4724b
Sudhanshu Narayane <sudhanshunarayane@gmail.com>
2024-08-02 16:03:21 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# EMpower-ED
+elearning website , its  a mini project of our college
+https://samadkhan4.github.io/EMpower-ED
---
+++ script.js
@@ -0,0 +1,12 @@
+new fullpage('#fullpage', {
+    autoScrolling: true,
+    navigation: true,
+})
+
+if (screen && screen.width > 991) {
+    document.write('<script type="text/javascript" src="fullpage.js"><\/script>');
+}
+
+$(document).ready(function () {
+    $('#fullpage').fullpage();
+});
